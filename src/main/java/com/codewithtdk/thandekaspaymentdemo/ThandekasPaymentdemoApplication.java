58f53b4d79380b6776179300f272565f280fe643
Thandeka <106659229+Thandeka-Kate@users.noreply.github.com>
2025-11-20 10:31:52 +0200

    first push

---
+++ src/main/java/com/codewithtdk/thandekaspaymentdemo/ThandekasPaymentdemoApplication.java
@@ -0,0 +1,13 @@
+package com.codewithtdk.thandekaspaymentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThandekasPaymentdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThandekasPaymentdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithtdk/thandekaspaymentdemo/ThandekasPaymentdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithtdk.thandekaspaymentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThandekasPaymentdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
