f93e467e84b0f911be77711cfddfab7604e313b9
Thandeka <106659229+Thandeka-Kate@users.noreply.github.com>
2025-11-20 10:39:41 +0200

    first push

--- src/main/java/com/codewithtdk/thandekaspaymentdemo/ThandekasPaymentdemoApplication.java
+++ src/main/java/com/codewithtdk/thandekaspaymentdemo/ThandekasPaymentdemoApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ThandekasPaymentdemoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ThandekasPaymentdemoApplication.class, args);
+        System.out.println("ThandekasPaymentdemoApplication started");
     }
+
 
 }
